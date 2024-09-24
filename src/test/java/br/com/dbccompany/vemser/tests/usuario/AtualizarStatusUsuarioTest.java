a1aaa887ee457876858696270db4d61e2120c89b
Carlos Henrique Alexandre <carloshenrique.charq@gmail.com>
2024-09-24 01:12:54 -0300

    refactor: testes de atualizar status.

--- src/test/java/br/com/dbccompany/vemser/tests/usuario/AtualizarStatusUsuarioTest.java
+++ src/test/java/br/com/dbccompany/vemser/tests/usuario/AtualizarStatusUsuarioTest.java
@@ -1,15 +1,13 @@
 package br.com.dbccompany.vemser.tests.usuario;
 
-import client.auth.AuthClient;
 import client.usuario.UsuarioClient;
-import io.restassured.response.Response;
+import models.usuario.UsuarioModel;
+import net.datafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import utils.auth.Auth;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,23 +15,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Endpoint de atualizar status usuário")
 public class AtualizarStatusUsuarioTest {
-
     private final UsuarioClient usuarioClient = new UsuarioClient();
-
-    @BeforeEach
-    public void setUp(){
-        Auth.usuarioGestaoDePessoas();
-    }
+    private final Faker faker = new Faker();
     @Test
     @DisplayName("Cenário 1: Deve atualizar status para desativo do gestor por id")
     @Tag("Regression")
     public void testDeveAtualizarStatusDesativoGestorPorId(){
-        Response response = usuarioClient.desativarContaGestor(AuthClient.getToken(), "3");
-
+        UsuarioModel usuarioAtualizado = usuarioClient.desativarContaGestor("3", true)
+                .then()
+                    .statusCode(HttpStatus.SC_OK)
+                    .extract()
+                    .as(UsuarioModel.class);
         assertAll(
-                () -> assertEquals("3", response.path("idGestor").toString()),
-                () -> assertEquals("F", response.path("ativo").toString()),
-                () -> assertNotNull(response.path("email"))
+                () -> assertEquals(3, usuarioAtualizado.getIdGestor()),
+                () -> assertEquals("F", usuarioAtualizado.getAtivo()),
+                () -> assertNotNull(usuarioAtualizado.getEmail()),
+                () -> assertNotNull(usuarioAtualizado.getCargosDto())
         );
     }
 
@@ -41,7 +38,7 @@ public class AtualizarStatusUsuarioTest {
     @DisplayName("Cenário 2: Tentar atualizar status para desativo do gestor com id nulo")
     @Tag("Regression")
     public void testTentarAtualizarStatusDesativoGestorPorIdNulo(){
-        usuarioClient.deletarGestor(AuthClient.getToken(), StringUtils.EMPTY)
+        usuarioClient.desativarContaGestor(StringUtils.EMPTY, true)
                 .then()
                     .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
     }
@@ -49,17 +46,17 @@ public class AtualizarStatusUsuarioTest {
     @Test
     @DisplayName("Cenário 3: Tentar atualizar status para desativo do gestor com id negativo")
     public void testTentarAtualizarStatusDesativoGestorPorIdNegativo(){
-        usuarioClient.deletarGestor(AuthClient.getToken(), "-1")
+        usuarioClient.desativarContaGestor(String.valueOf(faker.number().numberBetween(-100, -1)), true)
                 .then()
                     .body("message", equalTo("Usuario não encontrado!"))
                     .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 
     @Test
-    @DisplayName("Cenário 4: Tentar atualizar status para desativo do gestor com token inválido")
+    @DisplayName("Cenário 4: Tentar atualizar status para desativo do gestor sem token")
     @Tag("Regression")
-    public void testTentarAtualizarStatusDesativoGestorTokenInvalido(){
-        usuarioClient.deletarGestor(StringUtils.EMPTY, "3")
+    public void testTentarAtualizarStatusDesativoGestorSemToken(){
+        usuarioClient.desativarContaGestor("3", false)
                 .then()
                     .statusCode(HttpStatus.SC_FORBIDDEN);
     }
@@ -67,7 +64,7 @@ public class AtualizarStatusUsuarioTest {
     @Test
     @DisplayName("Cenário 5: Validar schema desativar gestor")
     public void testValidarSchemaDesativarGestor(){
-        usuarioClient.desativarContaGestor(AuthClient.getToken(), "3")
+        usuarioClient.desativarContaGestor( "3", true)
                 .then()
                     .body(matchesJsonSchemaInClasspath("schemas/usuario/Desativar_conta_gestor.json"))
                     .statusCode(HttpStatus.SC_OK);
