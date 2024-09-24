5a2bd51c2128c8a6cd99533db3c1af93a4eb51dc
Carlos Henrique Alexandre <carloshenrique.charq@gmail.com>
2024-09-24 01:12:31 -0300

    refactor: testes de deletar usuario.

--- src/test/java/br/com/dbccompany/vemser/tests/usuario/DeletarUsuarioTest.java
+++ src/test/java/br/com/dbccompany/vemser/tests/usuario/DeletarUsuarioTest.java
@@ -1,32 +1,22 @@
 package br.com.dbccompany.vemser.tests.usuario;
 
-import client.auth.AuthClient;
 import client.usuario.UsuarioClient;
+import net.datafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import utils.auth.Auth;
 
 import static org.hamcrest.Matchers.equalTo;
 
 @DisplayName("Endpoint de deletar usuário")
 public class DeletarUsuarioTest {
-
-    private UsuarioClient usuarioClient = new UsuarioClient();
-
-    @BeforeEach
-    public void setUp(){
-        Auth.usuarioGestaoDePessoas();
-    }
-
+    private final UsuarioClient usuarioClient = new UsuarioClient();
+    private final Faker faker = new Faker();
     @Test
     @DisplayName("Cenário 1: tentar deletar gestor com id nulo")
-    @Tag("Regression")
     public void testTentarDeletarGestorComIdNulo(){
-        usuarioClient.deletarGestor(AuthClient.getToken(), StringUtils.EMPTY)
+        usuarioClient.deletarGestor(StringUtils.EMPTY, true)
                 .then()
                     .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
     }
@@ -34,17 +24,16 @@ public class DeletarUsuarioTest {
     @Test
     @DisplayName("Cenário 2: tentar deletar gestor com id negativo")
     public void testTentarDeletarGestorComIdNegativo(){
-        usuarioClient.deletarGestor(AuthClient.getToken(), "-1")
+        usuarioClient.deletarGestor(String.valueOf(faker.number().numberBetween(-100, -1)), true)
                 .then()
                     .statusCode(HttpStatus.SC_BAD_REQUEST)
                     .body("message", equalTo("Usuario não encontrado!"));
     }
 
     @Test
-    @DisplayName("Cenário 3: tentar deletar gestor com token inválido")
-    @Tag("Regression")
-    public void testTentarDeletarGestorComTokenInvalido(){
-        usuarioClient.deletarGestor(StringUtils.EMPTY, "1")
+    @DisplayName("Cenário 3: tentar deletar gestor sem token")
+    public void testTentarDeletarGestorSemToken(){
+        usuarioClient.deletarGestor("1", false)
                 .then()
                     .statusCode(HttpStatus.SC_FORBIDDEN);
     }
