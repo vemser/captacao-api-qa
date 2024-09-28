953e9711765ed5e2e363cb95e4bb2683458c098f
Carlos Henrique Alexandre <carloshenrique.charq@gmail.com>
2024-09-28 12:01:57 -0300

    refactor: testes de listar avaliacao.
    obs: refatorando contrato de listar toda avaliacao.

--- src/test/java/br/com/dbccompany/vemser/tests/avaliacao/ListarAvaliacaoTest.java
+++ src/test/java/br/com/dbccompany/vemser/tests/avaliacao/ListarAvaliacaoTest.java
@@ -11,9 +11,7 @@ import models.avaliacao.AvaliacaoModel;
 import models.candidato.CandidatoCriacaoResponseModel;
 import models.inscricao.InscricaoModel;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.notNullValue;
@@ -25,93 +23,65 @@ class ListarAvaliacaoTest{
     private static final AvaliacaoClient avaliacaoClient = new AvaliacaoClient();
     private static final EdicaoClient edicaoClient = new EdicaoClient();
     private static final FormularioClient formularioClient = new FormularioClient();
-    @Test
-    @DisplayName("Cenário 1: Deve listar toda avaliação com sucesso")
-    @Tag("Regression")
-    public void testListarTodaAvaliacaoComSucesso() {
-        CandidatoCriacaoResponseModel candidatoCadastrado = candidatoClient.criarECadastrarCandidatoComCandidatoEntity()
+    private static CandidatoCriacaoResponseModel candidatoCadastrado;
+    private static InscricaoModel inscricaoCadastrada;
+    private static AvaliacaoModel avaliacaoCadastrada;
+    private static AvaliacaoCriacaoModel avaliacao;
+    @BeforeAll
+    public static void setUp(){
+        candidatoCadastrado = candidatoClient.criarECadastrarCandidatoComCandidatoEntity()
                 .then()
                     .statusCode(HttpStatus.SC_CREATED)
                     .extract()
                     .as(CandidatoCriacaoResponseModel.class);
-        InscricaoModel inscricaoCadastrada = inscricaoClient.cadastrarInscricao(candidatoCadastrado.getIdCandidato())
+        inscricaoCadastrada = inscricaoClient.cadastrarInscricao(candidatoCadastrado.getIdCandidato())
                 .then()
                     .statusCode(HttpStatus.SC_CREATED)
                     .extract()
                     .as(InscricaoModel.class);
-        AvaliacaoCriacaoModel avaliacao = AvaliacaoDataFactory.avaliacaoValida(inscricaoCadastrada.getIdInscricao(), true);
-        AvaliacaoModel avaliacaoCadastrada = avaliacaoClient.cadastrarAvaliacao(avaliacao, true)
+        avaliacao = AvaliacaoDataFactory.avaliacaoValida(inscricaoCadastrada.getIdInscricao(), true);
+        avaliacaoCadastrada = avaliacaoClient.cadastrarAvaliacao(avaliacao, true)
                 .then()
                     .statusCode(HttpStatus.SC_CREATED)
                     .extract()
                     .as(AvaliacaoModel.class);
+    }
+
+    @AfterAll
+    public static void setpDelete(){
+        avaliacaoClient.deletarAvaliacao(avaliacaoCadastrada.getIdAvaliacao(), true);
+        inscricaoClient.deletarInscricao(inscricaoCadastrada.getIdInscricao());
+        candidatoClient.deletarCandidato(candidatoCadastrado.getIdCandidato());
+        formularioClient.deletarFormulario(candidatoCadastrado.getFormulario().getIdFormulario());
+        edicaoClient.deletarEdicao(candidatoCadastrado.getEdicao().getIdEdicao());
+    }
+    @Test
+    @DisplayName("Cenário 1: Deve listar toda avaliação com sucesso")
+    @Tag("Regression")
+    public void testListarTodaAvaliacaoComSucesso() {
         avaliacaoClient.listarTodaAvaliacao(true)
                 .then()
                     .statusCode(HttpStatus.SC_OK)
                         .body("totalElementos", notNullValue())
                         .body("elementos", notNullValue());
-        avaliacaoClient.deletarAvaliacao(avaliacaoCadastrada.getIdAvaliacao(), true);
-        edicaoClient.deletarEdicao(candidatoCadastrado.getEdicao().getIdEdicao());
-        formularioClient.deletarFormulario(candidatoCadastrado.getFormulario().getIdFormulario());
-        candidatoClient.deletarCandidato(candidatoCadastrado.getIdCandidato());
     }
 
     @Test
     @DisplayName("Cenário 2: Tentar listar toda avaliação sem autenticação")
     @Tag("Regression")
     public void testTentarListarTodaAvaliacaoSemAutenticacao() {
-        CandidatoCriacaoResponseModel candidatoCadastrado = candidatoClient.criarECadastrarCandidatoComCandidatoEntity()
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(CandidatoCriacaoResponseModel.class);
-        InscricaoModel inscricaoCadastrada = inscricaoClient.cadastrarInscricao(candidatoCadastrado.getIdCandidato())
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(InscricaoModel.class);
-        AvaliacaoCriacaoModel avaliacao = AvaliacaoDataFactory.avaliacaoValida(inscricaoCadastrada.getIdInscricao(), true);
-        AvaliacaoModel avaliacaoCadastrada = avaliacaoClient.cadastrarAvaliacao(avaliacao, true)
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(AvaliacaoModel.class);
         avaliacaoClient.listarTodaAvaliacao(false)
                 .then()
                     .statusCode(HttpStatus.SC_FORBIDDEN);
-        avaliacaoClient.deletarAvaliacao(avaliacaoCadastrada.getIdAvaliacao(), true);
-        edicaoClient.deletarEdicao(candidatoCadastrado.getEdicao().getIdEdicao());
-        formularioClient.deletarFormulario(candidatoCadastrado.getFormulario().getIdFormulario());
-        candidatoClient.deletarCandidato(candidatoCadastrado.getIdCandidato());
     }
 
     @Test
-    @DisplayName("Cenário 3: Validar schema listar toda avaliação")
-    @Tag("Regression")
-    public void testValidarSchemaListarTodaAvaliacao(){
-        CandidatoCriacaoResponseModel candidatoCadastrado = candidatoClient.criarECadastrarCandidatoComCandidatoEntity()
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(CandidatoCriacaoResponseModel.class);
-        InscricaoModel inscricaoCadastrada = inscricaoClient.cadastrarInscricao(candidatoCadastrado.getIdCandidato())
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(InscricaoModel.class);
-        AvaliacaoCriacaoModel avaliacao = AvaliacaoDataFactory.avaliacaoValida(inscricaoCadastrada.getIdInscricao(), true);
-        AvaliacaoModel avaliacaoCadastrada = avaliacaoClient.cadastrarAvaliacao(avaliacao, true)
-                .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .extract()
-                    .as(AvaliacaoModel.class);
+    @DisplayName("Cenário 3: Validar contrato listar toda avaliação")
+    @Tag("Contract")
+    public void testValidarContratoListarTodaAvaliacao(){
         avaliacaoClient.listarTodaAvaliacao(true)
                 .then()
                     .statusCode(HttpStatus.SC_OK)
                     .body(matchesJsonSchemaInClasspath("schemas/avaliacao/Listar_toda_avaliacao.json"));
-        avaliacaoClient.deletarAvaliacao(avaliacaoCadastrada.getIdAvaliacao(), true);
-        edicaoClient.deletarEdicao(candidatoCadastrado.getEdicao().getIdEdicao());
-        formularioClient.deletarFormulario(candidatoCadastrado.getFormulario().getIdFormulario());
-        candidatoClient.deletarCandidato(candidatoCadastrado.getIdCandidato());
     }
 }
